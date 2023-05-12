89aae439aa9cf77d55e808711c06abd439f6c8b5
Asatulla <debixxter@gmail.com>
2023-05-12 17:06:50 +0600

    fix

--- src/utils/helpers/format-status-text.ts
+++ src/utils/helpers/format-status-text.ts
@@ -16,16 +16,3 @@ export default function (status: EMembershipStatus): string {
 			return 'Pending'
 	}
 }
-
-import { client } from '../../client'
-import { IEvent } from '@/types'
-import { eventAdapter } from './adapters'
-
-export default function (id: number): Promise<IEvent> {
-	return client
-		.get(`/events/${id}`)
-		.then(res => eventAdapter(res.data.data))
-		.catch(err => {
-			throw err
-		})
-}
